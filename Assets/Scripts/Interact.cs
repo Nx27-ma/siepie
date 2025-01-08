955bc665371c314f409a0aa5c21261c7ed9ffc3b
DavidOmegaS <119925608+DavidOmegaS@users.noreply.github.com>
2025-01-08 13:05:01 +0100

    test

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -6,17 +6,27 @@ using UnityEngine.Events;
 public class Interact : MonoBehaviour
 {
     public KeyCode interactbutton;
-    public Event Interactions;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            Debug.Log("test");
+
+        }
     }
 }
+
