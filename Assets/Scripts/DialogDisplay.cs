24e1a5fc2c9ace861a37a241be95766b49db17e1
Nora_de_Greeuw <146176774+Nx27-ma@users.noreply.github.com>
2025-01-09 14:58:08 +0100

    [NOT TESTED] Dialog should now conform to one string if it has the same UID

--- Assets/Scripts/DialogDisplay.cs
+++ Assets/Scripts/DialogDisplay.cs
@@ -7,9 +7,7 @@ public class DialogDisplay : MonoBehaviour
   public static TextAsset textAsset;
   void Start()
   {
-    textAsset = Resources.Load<TextAsset>("DialogData");
-    JsonDialogReader.DialogContainer container = JsonDialogReader.RequestDialogSet(textAsset);
-    print(container.Dialog);
+   
   }
 
   // Update is called once per frame
--- Assets/Scripts/JsonDialogReader.cs
+++ Assets/Scripts/JsonDialogReader.cs
@@ -1,13 +1,46 @@
+using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 
 public class JsonDialogReader : ScriptableObject
 {
-  public static DialogContainer RequestDialogSet(TextAsset textAsset)
+  public static List<string> DialogLines { get; private set; }
+  public static void UpdateDialogSet(int UID)
   {
-    string jsonString = textAsset.text;
-    return JsonUtility.FromJson<DialogContainer>(jsonString);
+    TextAsset[] allLoadedJsonFiles = Resources.LoadAll<TextAsset>("DialogData");
+    Debug.Log(allLoadedJsonFiles.Length);
+    List<DialogContainer> allDialogContainers = new();
+    foreach (TextAsset asset in allLoadedJsonFiles)
+    {
+      allDialogContainers.Add(JsonUtility.FromJson<DialogContainer>(asset.text));
+    }
+
+    getCurrentDialogSet(allDialogContainers);
+  //  DialogLines = orderDialogStrings();
   }
+
+  static void getCurrentDialogSet(List<DialogContainer> dialogContainers)
+  {
+    StringBuilder tempDialog = new();
+    foreach (DialogContainer dialogContainer in dialogContainers)
+    {
+       tempDialog.Append(dialogContainer.Dialog);
+    }
+  }
+  
+
+  //private static List<string> orderDialogStrings()
+  //{
+  //  if()
+  //  foreach (DialogContainer dialogContainer in CurrentDialogContainers)
+  //  {
+      
+  //  }
+  //  return 
+  //}
+
+
   [System.Serializable]
   public class DialogContainer
   {
