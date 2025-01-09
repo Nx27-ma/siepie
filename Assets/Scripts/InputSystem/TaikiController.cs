a8672d768d0e6e3598a417bfdba3278eb340f20d
Henry Dickman <120007720+SlimeMakesItLookEasy@users.noreply.github.com>
2025-01-09 10:21:24 +0100

    Conventions and comments
    Making the code look nice :)

--- Assets/Scripts/InputSystem/TaikiController.cs
+++ Assets/Scripts/InputSystem/TaikiController.cs
@@ -6,24 +6,26 @@ using UnityEngine.InputSystem;
 
 public class TaikiController : MonoBehaviour
 {
-    Rigidbody2D m_Rigidbody;
+    Rigidbody2D rb;
     public float MoveSpeed = 2f;
     Vector2 moveValue;
 
+    //Neccesary components fetched on enable -Henry
     private void OnEnable()
     {
-        m_Rigidbody = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
     //Function is called when Takkie receives the OnTakkieMove input from the PlayerInput component
-    //Value of the Vector2 value is read as Value
+    //Value of the Vector2 value is read as Value -Henry
     public void OnMove(InputValue value)
     {
         moveValue = value.Get<Vector2>();
     }
 
+    //Velocity is changed based on the value of the vector received to moveValue during OnMove -Henry
     private void FixedUpdate()
     {
         Vector2 move = transform.up * moveValue.y * MoveSpeed + transform.right * moveValue.x * MoveSpeed;
-        m_Rigidbody.velocity = move;
+        rb.velocity = move;
     }
 }
