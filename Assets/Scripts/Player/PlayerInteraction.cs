f16cb3f740d527de1392c8fa71cb5ea7019b1255
DavidOmegaS <119925608+DavidOmegaS@users.noreply.github.com>
2025-01-10 13:39:45 +0100

    Revert "ss"
    This reverts commit 039d0e3773d16d2a774f72c9a9fddad9337fed6e.

--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -14,7 +14,7 @@ public class PlayerInteraction : MonoBehaviour
 
     private void Awake()
     {
-        ActionAsset = Resources.Load<InputActionAsset>("Input System/ActionMap");
+        ActionAsset = Resources.Load<InputActionAsset>("Input System/PlayerControls");
         interact = ActionAsset.FindAction("Interact");
     }
 
