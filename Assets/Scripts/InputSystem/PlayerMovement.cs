703e27fcbc9dd4d52851f0d941ddc870b25af7f4
Henry Dickman <120007720+SlimeMakesItLookEasy@users.noreply.github.com>
2025-01-14 13:50:59 +0100

    Animator controller made
    Basic animation controller for the player is built. Going to pester people for all the neccesary animations to actually "finish" the player animations.

--- Assets/Scripts/InputSystem/PlayerMovement.cs
+++ Assets/Scripts/InputSystem/PlayerMovement.cs
@@ -6,13 +6,17 @@ using UnityEngine.InputSystem;
 
 public class TakkieMovement : MonoBehaviour
 {
+    int currentWalkAnim;
   public float MoveSpeed = 2f;
   Rigidbody2D rb;
   Vector2 moveValue;
+  Animator animator;
+  Vector2 zero = new Vector2(0, 0);
 
   //Neccesary components fetched on enable -Henry
   private void OnEnable()
   {
+    animator = GetComponent<Animator>();
     rb = GetComponent<Rigidbody2D>();
   }
   //Function is called when Takkie receives the OnTakkieMove input from the PlayerInput component
@@ -25,7 +29,23 @@ public class TakkieMovement : MonoBehaviour
   //Velocity is changed based on the value of the vector received to moveValue during OnMove -Henry
   private void FixedUpdate()
   {
-    Vector2 move = transform.up * moveValue.y * MoveSpeed + transform.right * moveValue.x * MoveSpeed;
-    rb.velocity = move;
+    Vector2 move = transform.up * moveValue.y + transform.right * moveValue.x;
+    animator.SetFloat("X", moveValue.x);
+    animator.SetFloat("Y", moveValue.y);
+    if (move == zero)
+    {
+        animator.SetBool("Moving", false);
+    }
+    else
+    {
+        animator.SetBool("Moving", true);
+    }
+    rb.velocity = move * MoveSpeed;
+        Debug.Log(this.tag + ": " + move);
+  }
+
+  public void ChangeCurrentWalkAnim(int value)
+  {
+        animator.SetInteger("WalkAnim", value);
   }
 }
