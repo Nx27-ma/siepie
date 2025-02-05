0db26975a581c29469a7797ebec8cbf84dfb13ac
Nora_de_Greeuw <146176774+Nx27-ma@users.noreply.github.com>
2025-02-05 13:10:59 +0100

    trying to use foldout

--- Assets/Editor/DialogUI/DialogUnityUI.cs
+++ Assets/Editor/DialogUI/DialogUnityUI.cs
@@ -1,30 +1,40 @@
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class DialogUnityUI : EditorWindow
 {
-    [SerializeField]
-    private VisualTreeAsset m_VisualTreeAsset = default;
+  [SerializeField]
+  private VisualTreeAsset m_VisualTreeAsset = default;
+  List<string> foldoutNames = new List<string>();
 
-    [MenuItem("Window/DialogUnityUI")]
-    public static void ShowExample()
-    {
-        DialogUnityUI wnd = GetWindow<DialogUnityUI>();
-        wnd.titleContent = new GUIContent("DialogUnityUI");
-    }
 
-    public void CreateGUI()
-    {
-        // Each editor window contains a root VisualElement object
-        VisualElement root = rootVisualElement;
+  [MenuItem("Window/DialogUnityUI")]
+  public static void ShowExample()
+  {
+    DialogUnityUI wnd = GetWindow<DialogUnityUI>();
+    wnd.titleContent = new GUIContent("DialogUnityUI");
+  }
 
-        // VisualElements objects can contain other VisualElement following a tree hierarchy.
-        VisualElement label = new Label("Hello World! From C#");
-        root.Add(label);
+  public void CreateGUI()
+  {
+    // Each editor window contains a root VisualElement object
+    VisualElement root = rootVisualElement;
 
-        // Instantiate UXML
-        VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
-        root.Add(labelFromUXML);
-    }
+    // Instantiate UXML
+    VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
+    root.Add(labelFromUXML);
+
+
+    ListView namesListView = root.Query<ListView>("DataContainerUXML");
+    Foldout foldout = new();
+    foldout.text = "name";
+    foldoutNames.Add(foldout.name);
+    namesListView.itemsSource = foldoutNames;
+    //namesListView.bindItem
+    root.Add(namesListView);
+    
+
+  }
 }
