0b40b57595bc7e58cabb24a1d96fd48af7d39888
Nora_de_Greeuw <146176774+Nx27-ma@users.noreply.github.com>
2025-03-13 22:04:25 +0100

    switching to pc

--- Assets/Scripts/Dialog/JsonObjectLoader.cs
+++ Assets/Scripts/Dialog/JsonObjectLoader.cs
@@ -8,18 +8,16 @@ namespace Dialog
   public static class JsonObjectLoader
   {
     static List<DialogContainer> DialogFromJson = new();
-    static TextAsset[] JsonFiles;
+    static TextAsset JsonFile;
     public static void LoadCharacters()
     {
-      JsonFiles = Resources.LoadAll<TextAsset>("DialogData/Characters");
-      if (JsonFiles.Length == 0) Debug.LogError("No JSON files found in DialogData/Characters"); 
+      JsonFile = Resources.Load<TextAsset>("DialogData/Characters/TheCharacterFile");
+      if (JsonFile == null) Debug.LogError("No JSON file found in DialogData/Characters");
       DialogFromJson.Clear();
-      foreach (var jsonFile in JsonFiles)
-      {
-        var dialogContainer = JsonConvert.DeserializeObject<DialogContainer>(jsonFile.text);
-        DialogFromJson.Add(dialogContainer);
-        Debug.Log("DialogContainer: " + dialogContainer.Character);
-      }
+      var dialogContainer = JsonConvert.DeserializeObject<DialogContainer[]>(JsonFile.text);
+      DialogFromJson.Add(dialogContainer[0]);
+      Debug.Log("DialogContainer: " + dialogContainer[0].Character);
+
     }
   }
 }
